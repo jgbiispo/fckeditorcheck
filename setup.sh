47470bda0f697e91be9700fa2e8a490f8278bfd8
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-08-08 10:17:51 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_SRC="./fckeditor.py"
+
+INSTALL_DIR="/usr/local/bin"
+
+CMD_NAME="fckupload"
+
+if [ ! -f "$SCRIPT_SRC" ]; then
+  echo "[!] Arquivo $SCRIPT_SRC não encontrado. Coloque o setup.sh na mesma pasta do script ou ajuste o caminho."
+  exit 1
+fi
+
+sudo cp "$SCRIPT_SRC" "$INSTALL_DIR/$CMD_NAME.py"
+sudo chmod +x "$INSTALL_DIR/$CMD_NAME.py"
+
+echo "#!/bin/bash
+python3 $INSTALL_DIR/$CMD_NAME.py \"\$@\"
+" | sudo tee "$INSTALL_DIR/$CMD_NAME" > /dev/null
+
+sudo chmod +x "$INSTALL_DIR/$CMD_NAME"
+
+echo "[+] Instalado com sucesso!"
+echo "[+] Agora você pode usar o comando '$CMD_NAME' em qualquer lugar."
