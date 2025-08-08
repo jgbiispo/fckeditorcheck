7a4629e271b95b9ef5625a7360b83abdf1955e5e
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-08-08 10:21:49 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-SCRIPT_SRC="./fckeditor.py"
-
+SCRIPT_SRC="./fckeditor_checker_v0_1.py"
 INSTALL_DIR="/usr/local/bin"
-
 CMD_NAME="fckupload"
 
 if [ ! -f "$SCRIPT_SRC" ]; then
   echo "[!] Arquivo $SCRIPT_SRC não encontrado. Coloque o setup.sh na mesma pasta do script ou ajuste o caminho."
   exit 1
 fi
+echo "[!] USE COM RESPONSABILIDADE"
+
+echo "[*] Instalando dependência requests via pip (ignorando avisos)..."
+pip3 install --user --upgrade --disable-pip-version-check requests > /dev/null 2>&1
 
 sudo cp "$SCRIPT_SRC" "$INSTALL_DIR/$CMD_NAME.py"
 sudo chmod +x "$INSTALL_DIR/$CMD_NAME.py"
